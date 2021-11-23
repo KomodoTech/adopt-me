813d3ae86b5f38a4a65d99c509e4f989e682454c
KomodoTech <alexandre.leibler@gmail.com>
2021-11-23 15:22:13 -0500

    Set up eslint to work with JSX (new version that doesn't require React explicit import), and converted code from previous lesson to use JSX.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+//import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
@@ -7,6 +7,7 @@ import Pet from "./Pet";
 
 // The ... in ...[1,2,3,4] is the spread operator
 // the map function call a callback function on each element of an array
+/*
 const App = () => {
   return React.createElement("div", { id: "pets-display" }, [
     React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
@@ -28,3 +29,25 @@ const App = () => {
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
+*/
+
+// NOTE: by default eslint does not know how to handle JSX
+// It thinks that we never use the Pet element we imported even though we do
+// we need to "npm install -D eslint-plugin-import@2.22.1 eslint-plugin-jsx-a11y@6.4.1
+// eslint-plugin-react@7.22.0" the last one is the one that tells eslint how
+// to deal with JSX
+const App = () => {
+  const mapExample = [1, 2, 3, 4].map((i) => <h2 key={i}>{i}</h2>);
+
+  return (
+    <div id="pet-display">
+      <h1 id="my-brand">Adopt me!</h1>
+      {mapExample}
+      <Pet name="Bertrand" animal="Dog" breed="Havanese"></Pet>
+      <Pet name="Mr.Seeds" animal="Bird" breed="Lesser Paradise"></Pet>
+      <Pet name="Smeagol" animal="Cat" breed="Tabby"></Pet>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,25 @@
-import React from "react";
+// Babel knows to import React for us when it finds JSX
+//import React from "react";
 
+/*
 const Pet = (props) => {
   return React.createElement("div", { id: "pet-instance" }, [
     React.createElement("h2", { id: "pet-name" }, props.name),
     React.createElement("h3", { id: "pet-animal" }, props.animal),
     React.createElement("h3", { id: "pet-breed" }, props.breed),
   ]);
+};
+*/
+
+// JSX equivalent to above commented code:
+const Pet = (props) => {
+  return (
+    <div id="pet-instance">
+      <h2 id="pet-name">{props.name}</h2>
+      <h3 id="pet-animal">{props.animal}</h3>
+      <h3 id="pet-breed">{props.breed}</h3>
+    </div>
+  );
 };
 
 export default Pet;
